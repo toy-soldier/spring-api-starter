f5a1791442df758839d84a7d5c48505c723f2a2d
Sanjay Risbud <sanj19972001@yahoo.com>
2026-02-13 18:49:12 +0800

    exercise - product endpoints

--- src/main/java/com/codewithmosh/store/repositories/ProductRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/ProductRepository.java
@@ -1,7 +1,17 @@
 package com.codewithmosh.store.repositories;
 
 import com.codewithmosh.store.entities.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @EntityGraph(attributePaths = "category")
+    List<Product> findAllByCategoryId(Byte categoryId);
+
+    @Query("select p from Product p")
+    @EntityGraph(attributePaths = "category")
+    List<Product> findAllWithCategoryId();
 }
